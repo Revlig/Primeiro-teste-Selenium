b6778da5412572858de81a0ceae7add8188f5b7b
Sandman <31891672+Revlig@users.noreply.github.com>
2018-02-19 16:35:07 -0300

    Add files via upload
    Criada funções adicionais para deixar o código mais legível.

--- inputs.py
+++ inputs.py
@@ -1,10 +1,22 @@
-def names(tentativas):
-    names = ["carlos", "margot", "paulo", "caio", "vitor", "joao", "fabio",
+def name():
+    nomes = ["carlos", "margot", "paulo", "caio", "vitor", "joao", "fabio",
              "priscila", "raquel", "marcia", "katia"]
-    return names[tentativas]
+    return nomes
+
+
+def senha():
+    senhas = ["123mudar", "teste123", "senha123", "github123", "admin",
+              "passwd", "superuser", "oioto", "nove", "dez", "onze"]
+    return senhas
+
+
+def logins(tentativas):
+    return name()[tentativas]
 
 
 def passwords(tentativas):
-    passwords = ["123mudar", "teste123", "senha123", "github123", "admin",
-                 "passwd", "superuser", "oioto", "nove", "dez", "onze"]
-    return passwords[tentativas]
+    return senha()[tentativas]
+
+
+def names_lenght():
+    return (len(name()))
