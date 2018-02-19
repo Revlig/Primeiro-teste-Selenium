0515fa9e2d15882de68cfd5f9e983b84b598a104
Sandman <31891672+Revlig@users.noreply.github.com>
2018-02-19 15:57:02 -0300

    Inserção do arquivo inputs.py
    Contém os nomes e passwords a serem usados pelo arquivo "script.py" nos testes de login pelo Selenium.

---
+++ inputs.py
@@ -0,0 +1,10 @@
+def names(tentativas):
+    names = ["carlos", "margot", "paulo", "caio", "vitor", "joao", "fabio",
+             "priscila", "raquel", "marcia", "katia"]
+    return names[tentativas]
+
+
+def passwords(tentativas):
+    passwords = ["123mudar", "teste123", "senha123", "github123", "admin",
+                 "passwd", "superuser", "oioto", "nove", "dez", "onze"]
+    return passwords[tentativas]
